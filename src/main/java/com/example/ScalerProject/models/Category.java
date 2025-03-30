b599e1a58419043582ae8bf2a762c25c0c25d3e9
therishikant <therishikant@gmail.com>
2025-03-30 13:50:29 +0530

    Added JPA

--- src/main/java/com/example/ScalerProject/models/Category.java
+++ src/main/java/com/example/ScalerProject/models/Category.java
@@ -1,11 +1,20 @@
 package com.example.ScalerProject.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
-public class Category {
-    private long id;
-    private String name;
+@Entity
+public class Category extends BaseModel {
+    private String description;
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+    @OneToMany
+    private List<Product> featuredProducts;
 }
