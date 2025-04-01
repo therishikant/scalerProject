af50a994dcc956c9b716bf1452011cc3253cb114
therishikant <therishikant@gmail.com>
2025-04-01 11:07:05 +0530

    ProductRepository.java Updated

--- src/main/java/com/example/ScalerProject/repositories/ProductRepository.java
+++ src/main/java/com/example/ScalerProject/repositories/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.example.ScalerProject.repositories;
 
+import com.example.ScalerProject.models.Category;
 import com.example.ScalerProject.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +16,16 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findById(Long id);
 
     List<Product> findAll();
+
+    List<Product> findByCategory(Category category);
+
+    List<Product> findByCategory_Name(String categoryName);
+
+    @Query("select p from Product p where p.category.name = :categoryName")
+    List<Product> findByCategoryName(@Param("categoryName") String categoryName);
+
+    @Query(value = "select * from product where category_id in (select id from category where name = :categoryName)", nativeQuery = true)
+    List<Product> findByCategoryNameNative(@Param("categoryName") String categoryName);
+
+    
 }
